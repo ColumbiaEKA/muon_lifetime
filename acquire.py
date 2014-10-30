25f6cf17572fc50f96d6c5fcdbe95bd049557119
Daniel Flanigan <daniel.isaiah.flanigan@gmail.com>
2014-10-30 19:17:51 -0400

    Removed extraneous

--- acquire.py
+++ acquire.py
@@ -2,85 +2,97 @@ import os
 import sys
 import time
 import serial
+import pickle
 import lecroy
 
 def message(string):
     sys.stdout.write(string)
     sys.stdout.flush()
 
-def acquire(scope, directory, arm_attempts=10, acquire_attempts=10, poll_wait=0.5):
+def acquire(scope, directory, arm_attempts=10, acquire_attempts=10, poll_wait=0.5, pickle_all_data=False):
     scope.clear()
     sweep = 0
     try:
         # This is the main acquisition loop.
         while True:
-            message("Arming:")
+            # Tell the scope to arm and wait until it does.
+            message("Arming")
             scope.arm()
             arm_timeouts = 0
             while arm_timeouts < arm_attempts:
                 try:
                     internal = scope.internal
                     if internal[13]:
-                        message("Armed:")
+                        message("\rWaiting")
                         break
                 except lecroy.LeCroyTimeout:
-                    message('{}:'.format(arm_timeouts))
                     arm_timeouts += 1
             # If the scope fails to arm, try again
             else:
-                message("Failed to arm!\n")
+                message("\rFailed to arm!\n")
                 scope.stop()
                 continue
-            # Add timeout condition if necessary.
+
+            # Now that the scope is armed, wait for it to trigger.
+            # This loop never times out, so wait_timeouts is not used.
             wait_timeouts = 0
             while True:
                 try:
                     # Check state before re-acquiring as the scope may arm and trigger immediately.
                     if internal[0]:
-                        message("Triggered:")
+                        message("\rAcquiring waveform")
                         break
                     else:
                         time.sleep(poll_wait)
                         internal = scope.internal
-                        message(':')
                 except lecroy.LeCroyTimeout:
-                    message('{}:'.format(wait_timeouts))
                     wait_timeouts += 1
+
+            # Now that the scope has triggered, attept to acquire the waveform.
             acquire_timeouts = 0
             while acquire_timeouts < acquire_attempts:
                 try:
                     waveform = scope.parse_waveform(scope.waveform)
                     break
                 except lecroy.LeCroyTimeout:
-                    message('{}:'.format(acquire_timeouts))
                     acquire_timeouts += 1
             else:
-                message("Failed to acquire waveform!\n")
+                message("\rFailed to acquire waveform!\n")
                 continue
-            filename = 'sweep{:06}.txt'.format(sweep)
-            with open(os.path.join(directory, filename), 'w') as f:
-                f.writelines(['{}\n'.format(value) for value in waveform['data']])
-                message('Wrote {}\n'.format(filename))
+
+            # Write the data to disk.
+            if pickle_all_data:
+                pickle_filename = 'sweep{:06}.pkl'.format(sweep)
+                with open(os.path.join(directory, pickle_filename), 'w') as f:
+                    pickle.dump(waveform, f)
+            data_filename = 'sweep{:06}.txt'.format(sweep)
+            with open(os.path.join(directory, data_filename), 'w') as f:
+                f.writelines(['{:.5f}\n'.format(value) for value in waveform['voltage_waveform']])
+                message('\rWrote {}\n'.format(data_filename))
             sweep += 1
     except KeyboardInterrupt:
         scope.stop()
-        scope.auto_calibrate = True
         scope.local()
         scope.serial.close()
-        message("Stopped.\n")
+        message("\rStopped.\n")
 
-def main(base_directory):
-    port = 'COM1'
-    baudrate = 19200 #9600
-    timeout = 3
-    xonxoff = True
+
+def main(user_directory, base_directory='C:\Student Directories', port='COM1', baudrate=19200, timeout=3, xonxoff=True):
     serial_port = serial.Serial(port, baudrate=baudrate, timeout=timeout, xonxoff=xonxoff)
     scope = lecroy.LeCroy(serial_port)
     scope.remote()
-    print("Connected to {}".format(scope.identification))
-    scope.auto_calibrate = False
-    data_directory = os.path.join(base_directory, time.strftime('%Y%m%dT%H%M%S'))
-    os.mkdir(data_directory)
+    try:
+        print("Connected to {}".format(scope.identification))
+    except lecroy.LeCroyTimeout:
+        print("Failed to connect to scope. The scope baud rate must be {}.".format(baudrate))
+        sys.exit()
+    data_directory = os.path.join(base_directory, user_directory, time.strftime('%Y-%m-%d_%H%M%S'))
+    try:
+        os.mkdir(data_directory)
+    except OSError:
+        print("Failed to create directory {}".format(data_directory))
+        print("The directory {} must exist.".format(os.path.join(base_directory, user_directory)))
+        sys.exit()
     print("Writing to {}".format(data_directory))
     acquire(scope, data_directory)
 
--- lecroy.py
+++ lecroy.py
@@ -1,3 +1,5 @@
+import struct
+
 class LeCroy(object):
 
     program_message_terminator = '\r'
@@ -78,20 +80,29 @@ class LeCroy(object):
     def auto_calibrate(self, boolean):
         self.send('AUTO_CALIBRATE {}'.format(self.boolean_to_token[boolean]))
 
-    def _hex_bytes_to_int(self, bytes):
-        return int(''.join(bytes), 16)
+    @property
+    def waveform(self):
+        return self.send_and_receive('WAVEFORM?')
+
+    # These methods are used to parse the raw waveform data. They work only for big-endian data, meaning COMM_ORDER
+    # HI or comm_order = 0 in a waveform. This is the default. In little-endian order I think we would have to
+    # reverse the bit order in each nibble and pass the endian-ness to the float converter.
+
+    def _hex_bytes_to_binary_string(self, bytes):
+        binary_format_code = '{{:0{}b}}'.format(8 * len(bytes))
+        return binary_format_code.format(int(''.join(bytes), 16))
 
     def _hex_bytes_to_str(self, bytes):
         return ''.join([chr(int(b, 16)) for b in bytes])
 
     def _hex_bytes_to_signed_int(self, bytes):
-        binary_format_code = '{{:0{}b}}'.format(8 * len(bytes))
-        binary = binary_format_code.format(int(''.join(bytes), 16))
+        binary = self._hex_bytes_to_binary_string(bytes)
         return int(binary, 2) - int(binary[0], 2) * 2**len(binary)
 
-    @property
-    def waveform(self):
-        return self.send_and_receive('WAVEFORM?')
+    def _hex_bytes_to_float(self, bytes):
+        ascii = self._hex_bytes_to_str(bytes)
+        float_or_double = {4: 'f', 8: 'd'}[len(ascii)]
+        return struct.unpack('>'+float_or_double, ascii)[0]
 
     def parse_waveform(self, message):
         content = message.split('#', 1)[1]
@@ -104,32 +115,46 @@ class LeCroy(object):
         waveform = {}
         waveform['descriptor_name'] = self._hex_bytes_to_str(hex_bytes[:16])
         waveform['template_name'] = self._hex_bytes_to_str(hex_bytes[16:32])
-        waveform['comm_type'] = self._hex_bytes_to_int(hex_bytes[32:34])
-        waveform['comm_order'] = self._hex_bytes_to_int(hex_bytes[34:36])
-        waveform['wave_descriptor'] = self._hex_bytes_to_int(hex_bytes[36:40])
-        waveform['user_text'] = self._hex_bytes_to_int(hex_bytes[40:44])
-        waveform['res_desc1'] = self._hex_bytes_to_int(hex_bytes[44:48]) # This isn't documented
-        waveform['trigtime_array'] = self._hex_bytes_to_int(hex_bytes[48:52])
-        waveform['ris_time_array'] = self._hex_bytes_to_int(hex_bytes[52:56])
-        waveform['res_array1'] = self._hex_bytes_to_int(hex_bytes[56:60])
-        waveform['wave_array_1'] = self._hex_bytes_to_int(hex_bytes[60:64])
-        waveform['wave_array_2'] = self._hex_bytes_to_int(hex_bytes[64:68])
-        waveform['res_array2'] = self._hex_bytes_to_int(hex_bytes[68:72])
-        waveform['res_array3'] = self._hex_bytes_to_int(hex_bytes[72:76])
+        waveform['comm_type'] = self._hex_bytes_to_signed_int(hex_bytes[32:34])
+        waveform['comm_order'] = self._hex_bytes_to_signed_int(hex_bytes[34:36])
+        waveform['wave_descriptor'] = self._hex_bytes_to_signed_int(hex_bytes[36:40])
+        waveform['user_text'] = self._hex_bytes_to_signed_int(hex_bytes[40:44])
+        waveform['res_desc1'] = self._hex_bytes_to_signed_int(hex_bytes[44:48]) # This isn't documented
+        waveform['trigtime_array'] = self._hex_bytes_to_signed_int(hex_bytes[48:52])
+        waveform['ris_time_array'] = self._hex_bytes_to_signed_int(hex_bytes[52:56])
+        waveform['res_array1'] = self._hex_bytes_to_signed_int(hex_bytes[56:60])
+        waveform['wave_array_1'] = self._hex_bytes_to_signed_int(hex_bytes[60:64])
+        waveform['wave_array_2'] = self._hex_bytes_to_signed_int(hex_bytes[64:68])
+        waveform['res_array2'] = self._hex_bytes_to_signed_int(hex_bytes[68:72])
+        waveform['res_array3'] = self._hex_bytes_to_signed_int(hex_bytes[72:76])
         waveform['instrument_name'] = self._hex_bytes_to_str(hex_bytes[76:92])
-        waveform['instrument_number'] = self._hex_bytes_to_int(hex_bytes[92:96])
+        waveform['instrument_number'] = self._hex_bytes_to_signed_int(hex_bytes[92:96])
         waveform['trace_label'] = self._hex_bytes_to_str(hex_bytes[96:112])
         #
-        waveform['wave_array_count'] = self._hex_bytes_to_int(hex_bytes[116:120])
-        waveform['points_per_screen'] = self._hex_bytes_to_int(hex_bytes[120:124])
+        waveform['wave_array_count'] = self._hex_bytes_to_signed_int(hex_bytes[116:120])
+        waveform['points_per_screen'] = self._hex_bytes_to_signed_int(hex_bytes[120:124])
         #
-        waveform['timebase'] = self._hex_bytes_to_int(hex_bytes[324:326])
+        waveform['vertical_gain'] = self._hex_bytes_to_float(hex_bytes[156:160])
+        waveform['vertical_offset'] = self._hex_bytes_to_float(hex_bytes[160:164])
+        waveform['max_value'] = self._hex_bytes_to_float(hex_bytes[164:168])
+        waveform['min_value'] = self._hex_bytes_to_float(hex_bytes[168:172])
+        waveform['nominal_bits'] = self._hex_bytes_to_signed_int(hex_bytes[172:174])
+        waveform['nom_subarray_count'] = self._hex_bytes_to_signed_int(hex_bytes[174:176])
+        waveform['horiz_interval'] = self._hex_bytes_to_float(hex_bytes[176:180])
+        waveform['horiz_offset'] = self._hex_bytes_to_float(hex_bytes[180:188])
+        waveform['pixel_offset'] = self._hex_bytes_to_float(hex_bytes[188:196])
+        waveform['vertunit'] = self._hex_bytes_to_str(hex_bytes[196:244])
+        waveform['horunit'] = self._hex_bytes_to_str(hex_bytes[244:292])
         #
-        waveform['fixed_vert_gain'] = self._hex_bytes_to_int(hex_bytes[332:334])
-        waveform['wave_source'] = self._hex_bytes_to_int(hex_bytes[344:346])
-        data = hex_bytes[waveform['wave_descriptor'] + waveform['user_text']:]
-        waveform['data'] = [self._hex_bytes_to_signed_int((high, low))
-                            for high, low in zip(data[::2], data[1::2])]
+        waveform['timebase'] = self._hex_bytes_to_signed_int(hex_bytes[324:326])
+        #
+        waveform['fixed_vert_gain'] = self._hex_bytes_to_signed_int(hex_bytes[332:334])
+        waveform['wave_source'] = self._hex_bytes_to_signed_int(hex_bytes[344:346])
+        waveform_hex_bytes = hex_bytes[waveform['wave_descriptor'] + waveform['user_text']:]
+        waveform['adc_waveform'] = [self._hex_bytes_to_signed_int((high, low))
+                                    for high, low in zip(waveform_hex_bytes[::2], waveform_hex_bytes[1::2])]
+        waveform['voltage_waveform'] = [waveform['vertical_gain'] * adc - waveform['vertical_offset']
+                                        for adc in waveform['adc_waveform']]
         return waveform
 
 
